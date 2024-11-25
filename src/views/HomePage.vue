9518652bd9be2bff87f7fb33b6b5460577556fdf
Erhaes <rassyahafizh@gmail.com>
2024-11-25 20:30:05 +0700

    apalah

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -7,115 +7,176 @@
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <!-- komponen paling atas dari ion-content -->
-      <ion-refresher slot="fixed" :pull-factor="0.5" :pull-min="100" :pull-max="200"
-        @ionRefresh="handleRefresh($event)">
-        <ion-refresher-content></ion-refresher-content>
-      </ion-refresher>
-  
-      <!-- bagian refresher -->
+      <!-- modifikasi src/views/HomePage.vue pada bagian ion-content dalam template -->
+      <ion-content :fullscreen="true">
+        <!-- komponen paling atas dari ion-content -->
+        <ion-refresher
+          slot="fixed"
+          :pull-factor="0.5"
+          :pull-min="100"
+          :pull-max="200"
+          @ionRefresh="handleRefresh($event)">
+          <ion-refresher-content></ion-refresher-content>
+        </ion-refresher>
 
-<!-- active todos -->
-<div class="scrollable-container">
-  <ion-list>
-    <ion-item-sliding v-for="todo in activeTodos" :key="todo.id" :ref="(el) => setItemRef(el, todo.id!)">
-      <ion-item-options side="start" @ionSwipe="handleDelete(todo)">
-        <ion-item-option color="danger" expandable @click="handleDelete(todo)">
-          <ion-icon slot="icon-only" :icon="trash" size="large"></ion-icon>
-        </ion-item-option>
-      </ion-item-options>
+        <!-- komponen utama di antara 2 komponen ini -->
+        <!-- bagian refresher -->
 
-      <ion-item>
-        <ion-card>
-          <ion-card-header>
-            <ion-card-title class="ion-text-wrap limited-text">{{ todo.title }}</ion-card-title>
-            <ion-card-subtitle class="limited-text">{{ todo.description }}</ion-card-subtitle>
-          </ion-card-header>
+        <!-- active todos -->
+        <div class="scrollable-container">
+          <ion-list>
+            <ion-item-sliding
+              v-for="todo in activeTodos"
+              :key="todo.id"
+              :ref="(el) => setItemRef(el, todo.id!)">
+              <ion-item-options side="start" @ionSwipe="handleDelete(todo)">
+                <ion-item-option
+                  color="danger"
+                  expandable
+                  @click="handleDelete(todo)">
+                  <ion-icon
+                    slot="icon-only"
+                    :icon="trash"
+                    size="large"></ion-icon>
+                </ion-item-option>
+              </ion-item-options>
 
-          <ion-card-content>
-            <ion-badge>{{ getRelativeTime(todo.updatedAt) }}</ion-badge>
-          </ion-card-content>
-        </ion-card>
-      </ion-item>
+              <ion-item>
+                <ion-card>
+                  <ion-card-header>
+                    <ion-card-title class="ion-text-wrap limited-text">{{
+                      todo.title
+                    }}</ion-card-title>
+                    <ion-card-subtitle class="limited-text">{{
+                      todo.description
+                    }}</ion-card-subtitle>
+                  </ion-card-header>
 
-      <ion-item-options side="end" @ionSwipe="handleStatus(todo)">
-        <ion-item-option @click="handleEdit(todo)">
-          <ion-icon slot="icon-only" :icon="create" size="large"></ion-icon>
-        </ion-item-option>
-        <ion-item-option color="success" expandable @click="handleStatus(todo)">
-          <ion-icon slot="icon-only" :icon="checkmarkCircle" color="light" size="large"></ion-icon>
-        </ion-item-option>
-      </ion-item-options>
-    </ion-item-sliding>
-    <ion-item v-if="activeTodos.length === 0" class="ion-text-center">
-      <ion-label>No active todos</ion-label>
-    </ion-item>
-  </ion-list>
-</div>
+                  <ion-card-content>
+                    <ion-badge>{{ getRelativeTime(todo.updatedAt) }}</ion-badge>
+                  </ion-card-content>
+                </ion-card>
+              </ion-item>
 
-<!-- completed todos -->
-<ion-item class="accordion-container">
-  <ion-accordion-group>
-    <ion-accordion value="first">
-      <ion-item slot="header" color="light">
-        <ion-label class="ion-text-center">Completed</ion-label>
-      </ion-item>
-      <div slot="content" class="scrollable-container">
-        <ion-list>
-          <ion-item-sliding v-for="todo in completedTodos" :key="todo.id" :ref="(el) => setItemRef(el, todo.id!)">
-            <ion-item-options side="start" @ionSwipe="handleDelete(todo)">
-              <ion-item-option color="danger" expandable @click="handleDelete(todo)">
-                <ion-icon slot="icon-only" :icon="trash" size="large"></ion-icon>
-              </ion-item-option>
-            </ion-item-options>
-
-            <ion-item>
-              <ion-card>
-                <ion-card-header>
-                  <ion-card-title class="ion-text-wrap limited-text">{{ todo.title }}</ion-card-title>
-                  <ion-card-subtitle class="limited-text">{{ todo.description }}</ion-card-subtitle>
-                </ion-card-header>
-
-                <ion-card-content>
-                  <ion-badge>{{ getRelativeTime(todo.updatedAt) }}</ion-badge>
-                </ion-card-content>
-              </ion-card>
+              <ion-item-options side="end" @ionSwipe="handleStatus(todo)">
+                <ion-item-option @click="handleEdit(todo)">
+                  <ion-icon
+                    slot="icon-only"
+                    :icon="create"
+                    size="large"></ion-icon>
+                </ion-item-option>
+                <ion-item-option
+                  color="success"
+                  expandable
+                  @click="handleStatus(todo)">
+                  <ion-icon
+                    slot="icon-only"
+                    :icon="checkmarkCircle"
+                    color="light"
+                    size="large"></ion-icon>
+                </ion-item-option>
+              </ion-item-options>
+            </ion-item-sliding>
+            <ion-item v-if="activeTodos.length === 0" class="ion-text-center">
+              <ion-label>No active todos</ion-label>
             </ion-item>
+          </ion-list>
+        </div>
 
-            <ion-item-options side="end" @ionSwipe="handleStatus(todo)">
-              <ion-item-option @click="handleEdit(todo)">
-                <ion-icon slot="icon-only" :icon="create" size="large"></ion-icon>
-              </ion-item-option>
-              <ion-item-option color="warning" expandable @click="handleStatus(todo)">
-                <ion-icon slot="icon-only" :icon="close" color="light" size="large"></ion-icon>
-              </ion-item-option>
-            </ion-item-options>
-          </ion-item-sliding>
-          <ion-item v-if="completedTodos.length === 0" class="ion-text-center">
-            <ion-label>No completed todos</ion-label>
-          </ion-item>
-        </ion-list>
-      </div>
-    </ion-accordion>
-  </ion-accordion-group>
-</ion-item>
+        <!-- completed todos -->
+        <ion-item class="accordion-container">
+          <ion-accordion-group>
+            <ion-accordion value="first">
+              <ion-item slot="header" color="light">
+                <ion-label class="ion-text-center">Completed</ion-label>
+              </ion-item>
+              <div slot="content" class="scrollable-container">
+                <ion-list>
+                  <ion-item-sliding
+                    v-for="todo in completedTodos"
+                    :key="todo.id"
+                    :ref="(el) => setItemRef(el, todo.id!)">
+                    <ion-item-options
+                      side="start"
+                      @ionSwipe="handleDelete(todo)">
+                      <ion-item-option
+                        color="danger"
+                        expandable
+                        @click="handleDelete(todo)">
+                        <ion-icon
+                          slot="icon-only"
+                          :icon="trash"
+                          size="large"></ion-icon>
+                      </ion-item-option>
+                    </ion-item-options>
 
-<!-- bagian tombol dan modal -->
-      
-      <!-- komponen paling bawah dari ion-content -->
-      <ion-fab vertical="bottom" horizontal="end" slot="fixed">
-        <ion-fab-button @click="isOpen = true;">
-          <ion-icon :icon="add" size="large"></ion-icon>
-        </ion-fab-button>
-      </ion-fab>
-      <InputModal v-model:isOpen="isOpen" v-model:editingId="editingId" :todo="todo" @submit="handleSubmit" />
-      <TabsMenu />
+                    <ion-item>
+                      <ion-card>
+                        <ion-card-header>
+                          <ion-card-title class="ion-text-wrap limited-text">{{
+                            todo.title
+                          }}</ion-card-title>
+                          <ion-card-subtitle class="limited-text">{{
+                            todo.description
+                          }}</ion-card-subtitle>
+                        </ion-card-header>
+
+                        <ion-card-content>
+                          <ion-badge>{{
+                            getRelativeTime(todo.updatedAt)
+                          }}</ion-badge>
+                        </ion-card-content>
+                      </ion-card>
+                    </ion-item>
+
+                    <ion-item-options side="end" @ionSwipe="handleStatus(todo)">
+                      <ion-item-option @click="handleEdit(todo)">
+                        <ion-icon
+                          slot="icon-only"
+                          :icon="create"
+                          size="large"></ion-icon>
+                      </ion-item-option>
+                      <ion-item-option
+                        color="warning"
+                        expandable
+                        @click="handleStatus(todo)">
+                        <ion-icon
+                          slot="icon-only"
+                          :icon="close"
+                          color="light"
+                          size="large"></ion-icon>
+                      </ion-item-option>
+                    </ion-item-options>
+                  </ion-item-sliding>
+                  <ion-item
+                    v-if="completedTodos.length === 0"
+                    class="ion-text-center">
+                    <ion-label>No completed todos</ion-label>
+                  </ion-item>
+                </ion-list>
+              </div>
+            </ion-accordion>
+          </ion-accordion-group>
+        </ion-item>
+
+        <!-- komponen paling bawah dari ion-content -->
+        <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+          <ion-fab-button @click="isOpen = true">
+            <ion-icon :icon="add" size="large"></ion-icon>
+          </ion-fab-button>
+        </ion-fab>
+        <InputModal
+          v-model:isOpen="isOpen"
+          v-model:editingId="editingId"
+          :todo="todo"
+          @submit="handleSubmit" />
+      </ion-content>
     </ion-content>
-
   </ion-page>
 </template>
 
 <script setup lang="ts">
+import TabsMenu from "@/components/TabsMenu.vue";
 import {
   IonContent,
   IonHeader,
@@ -142,8 +203,8 @@ import {
   loadingController,
   IonRefresher,
   IonRefresherContent,
-  toastController
-} from '@ionic/vue';
+  toastController,
+} from "@ionic/vue";
 import {
   add,
   checkmarkCircle,
@@ -151,23 +212,25 @@ import {
   create,
   trash,
   closeCircle,
-  warningOutline
-} from 'ionicons/icons';
-import InputModal from '@/components/InputModal.vue';
-import { onMounted, ref, computed, onUnmounted } from 'vue';
-import { firestoreService, type Todo } from '@/utils/firestore';
-import { formatDistanceToNow } from 'date-fns';
-import TabsMenu from '@/components/TabsMenu.vue';
+  warningOutline,
+} from "ionicons/icons";
+import InputModal from "@/components/InputModal.vue";
+import { onMounted, ref, computed, onUnmounted } from "vue";
+import { firestoreService, type Todo } from "@/utils/firestore";
+import { formatDistanceToNow } from "date-fns";
 
+// modifikasi src/views/HomePage.vue deklarasikan variabel yang akan digunakan
 const isOpen = ref(false);
 const editingId = ref<string | null>(null);
 const todos = ref<Todo[]>([]);
-const todo = ref<Omit<Todo, 'id' | 'createdAt' | 'updatedAt' | 'status'>>({
-  title: '',
-  description: '',
+const todo = ref<Omit<Todo, "id" | "createdAt" | "updatedAt" | "status">>({
+  title: "",
+  description: "",
 });
-const activeTodos = computed(() => todos.value.filter(todo => !todo.status));
-const completedTodos = computed(() => todos.value.filter(todo => todo.status));
+const activeTodos = computed(() => todos.value.filter((todo) => !todo.status));
+const completedTodos = computed(() =>
+  todos.value.filter((todo) => todo.status)
+);
 const itemRefs = ref<Map<string, HTMLIonItemSlidingElement>>(new Map());
 let intervalId: any;
 const timeUpdateTrigger = ref(0);
@@ -180,13 +243,17 @@ const setItemRef = (el: any, id: string) => {
 };
 
 // toast notifikasi
-const showToast = async (message: string, color: string = 'success', icon: string = checkmarkCircle) => {
+const showToast = async (
+  message: string,
+  color: string = "success",
+  icon: string = checkmarkCircle
+) => {
   const toast = await toastController.create({
     message,
     duration: 2000,
     color,
-    position: 'top',
-    icon
+    position: "top",
+    icon,
   });
   await toast.present();
 };
@@ -198,37 +265,40 @@ const getRelativeTime = (date: any) => {
     const jsDate = date?.toDate ? date.toDate() : new Date(date);
     return formatDistanceToNow(jsDate, { addSuffix: true });
   } catch (error) {
-    return 'Invalid date';
+    return "Invalid date";
   }
 };
 
-// fungsi handleRefresh dan handleSubmit akan didefinisikan nanti
+// handle swipe refresher
 const handleRefresh = async (event: any) => {
   try {
     await loadTodos(false);
   } catch (error) {
-    console.error('Error refreshing:', error);
+    console.error("Error refreshing:", error);
   } finally {
     event.target.complete();
   }
 };
 
-const handleSubmit = async (todo: Omit<Todo, 'id' | 'createdAt' | 'updatedAt' | 'status'>) => {
+// handle submit add/edit pada modal
+const handleSubmit = async (
+  todo: Omit<Todo, "id" | "createdAt" | "updatedAt" | "status">
+) => {
   if (!todo.title) {
-    await showToast('Title is required', 'warning', warningOutline);
+    await showToast("Title is required", "warning", warningOutline);
     return;
   }
   try {
     if (editingId.value) {
       await firestoreService.updateTodo(editingId.value, todo as Todo);
-      await showToast('Todo updated successfully', 'success', checkmarkCircle);
+      await showToast("Todo updated successfully", "success", checkmarkCircle);
     } else {
       await firestoreService.addTodo(todo as Todo);
-      await showToast('Todo added successfully', 'success', checkmarkCircle);
+      await showToast("Todo added successfully", "success", checkmarkCircle);
     }
     loadTodos();
   } catch (error) {
-    await showToast('An error occurred', 'danger', closeCircle);
+    await showToast("An error occurred", "danger", closeCircle);
     console.error(error);
   } finally {
     editingId.value = null;
@@ -240,7 +310,7 @@ const loadTodos = async (isLoading = true) => {
   let loading;
   if (isLoading) {
     loading = await loadingController.create({
-      message: 'Loading...'
+      message: "Loading...",
     });
     await loading.present();
   }
@@ -256,6 +326,49 @@ const loadTodos = async (isLoading = true) => {
   }
 };
 
+// handle edit click
+const handleEdit = async (editTodo: Todo) => {
+  const slidingItem = itemRefs.value.get(editTodo.id!);
+  await slidingItem?.close();
+
+  editingId.value = editTodo.id!;
+  todo.value = {
+    title: editTodo.title,
+    description: editTodo.description,
+  };
+  isOpen.value = true;
+};
+
+// handle delete click/swipe
+const handleDelete = async (deleteTodo: Todo) => {
+  try {
+    await firestoreService.deleteTodo(deleteTodo.id!);
+    await showToast("Todo deleted successfully", "success", checkmarkCircle);
+    loadTodos();
+  } catch (error) {
+    await showToast("Failed to delete todo", "danger", closeCircle);
+    console.error(error);
+  }
+};
+
+// handle status click/swipe, mengubah status todo active (false)/completed (true)
+const handleStatus = async (statusTodo: Todo) => {
+  const slidingItem = itemRefs.value.get(statusTodo.id!);
+  await slidingItem?.close();
+  try {
+    await firestoreService.updateStatus(statusTodo.id!, !statusTodo.status);
+    await showToast(
+      `Todo marked as ${!statusTodo.status ? "completed" : "active"}`,
+      "success",
+      checkmarkCircle
+    );
+    loadTodos();
+  } catch (error) {
+    await showToast("Failed to update status", "danger", closeCircle);
+    console.error(error);
+  }
+};
+
 // dijalankan setiap halaman diload, load data dan set interval update 1 menit
 onMounted(() => {
   loadTodos();
@@ -268,84 +381,10 @@ onMounted(() => {
 onUnmounted(() => {
   clearInterval(intervalId);
 });
-
-
-// handle edit click
-const handleEdit = async (editTodo: Todo) => {
-  const slidingItem = itemRefs.value.get(editTodo.id!);
-  await slidingItem?.close();
-
-  editingId.value = editTodo.id!;
-  todo.value = {
-    title: editTodo.title,
-    description: editTodo.description,
-  }
-  isOpen.value = true;
-}
-
-// handle delete click/swipe
-const handleDelete = async (deleteTodo: Todo) => {
-  try {
-    await firestoreService.deleteTodo(deleteTodo.id!);
-    await showToast('Todo deleted successfully', 'success', checkmarkCircle);
-    loadTodos();
-  } catch (error) {
-    await showToast('Failed to delete todo', 'danger', closeCircle);
-    console.error(error);
-  }
-};
-
-// handle status click/swipe, mengubah status todo active (false)/completed (true)
-const handleStatus = async (statusTodo: Todo) => {
-  const slidingItem = itemRefs.value.get(statusTodo.id!);
-  await slidingItem?.close();
-  try {
-    await firestoreService.updateStatus(statusTodo.id!, !statusTodo.status);
-    await showToast(
-      `Todo marked as ${!statusTodo.status ? 'completed' : 'active'}`,
-      'success',
-      checkmarkCircle
-    );
-    loadTodos();
-  } catch (error) {
-    await showToast('Failed to update status', 'danger', closeCircle);
-    console.error(error);
-  }
-};
-
-
 </script>
 
-
+<!-- modifikasi src/views/HomePage.vue tambahkan keseluruhan style -->
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-
 ion-card,
 ion-accordion-group {
   width: 100%;
